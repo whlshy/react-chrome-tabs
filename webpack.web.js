23ba745d06f97ca9d269014c026ca8ca79b5c9da
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2022-05-02 19:47:00 +0800

    web

---
+++ webpack.web.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge'),
+  common = require('./webpack.common.js'),
+  webpack = require('webpack'),
+  { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+  HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = merge(common, {
+  entry: ['./src/start.js'],
+  output: {
+    path: __dirname + '/docs/',
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/www/index.html`,
+      filename: "index.html",
+      inject: "body"
+    })
+  ],
+})
