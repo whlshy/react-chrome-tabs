71e69df3c287cd9da6b6ac7e28a350052f946f25
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2022-05-03 02:03:23 +0800

    change ico url

--- src/start.js
+++ src/start.js
@@ -3,12 +3,12 @@ import { render } from 'react-dom'
 import App from './components/Tabs.js'
 
 let tabs = [
-  { id: 0, favicon: "https://raw.githubusercontent.com/adamschwartz/chrome-tabs/gh-pages/demo/images/google-favicon.ico", title: "Google" },
-  { id: 1, favicon: "https://raw.githubusercontent.com/adamschwartz/chrome-tabs/gh-pages/demo/images/facebook-favicon.ico", title: "Facebook" },
+  { id: 0, favicon: "https://www.google.com/favicon.ico", title: "Google" },
+  { id: 1, favicon: "https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico", title: "Facebook" },
   { id: 2, favicon: "https://it108.wke.csie.ncnu.edu.tw/edu.ico", title: "IT Technology" }
 ]
 
 render(
-  <App />
+  <App defaultTabs={tabs} />
   , document.getElementById('root')
 )
