1a56b24bfa26c82f7d48fe3fca3481810624d998
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2022-05-06 16:16:27 +0800

    fix demo page

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -9,23 +9,23 @@ export default function Demo(props) {
     { key: 2, favicon: "https://it108.wke.csie.ncnu.edu.tw/edu.ico", title: "IT Technology" }
   ])
   const [dark, setDark] = useState(false)
-  const [id, setID] = useState(2)
+  const [key, setID] = useState(2)
   const [current, setCurrent] = useState(0)
 
   const addTab = (isCurrent) => {
-    let tab = [{ id: id + 1, title: "New Tab" }]
+    let tab = [{ key: key + 1, title: "New Tab" }]
     let tmpTabs = tabs.concat(tab);
-    !!isCurrent && setCurrent(id + 1)
-    setID(id + 1)
+    !!isCurrent && setCurrent(key + 1)
+    setID(key + 1)
     setTabs(tmpTabs)
   }
 
-  const onClose = (id) => {
-    let tmpTabs = tabs.filter(f => f.id !== id);
+  const onClose = (key) => {
+    let tmpTabs = tabs.filter(f => f.key !== key);
     let idx = null; 
-    tabs.forEach((f, index) => f.id === current && (idx = index))
-    let isCurrent = tmpTabs.filter(f => f.id === current).length > 0 ? true : false;
-    let tmpCurrent = tmpTabs.length > 0 ? (tmpTabs[idx] ? tmpTabs[idx].id : tmpTabs[tmpTabs.length - 1].id) : null;
+    tabs.forEach((f, index) => f.key === current && (idx = index))
+    let isCurrent = tmpTabs.filter(f => f.key === current).length > 0 ? true : false;
+    let tmpCurrent = tmpTabs.length > 0 ? (tmpTabs[idx] ? tmpTabs[idx].key : tmpTabs[tmpTabs.length - 1].key) : null;
     !isCurrent && setCurrent(tmpCurrent)
     setTabs(tmpTabs)
   }
@@ -39,8 +39,8 @@ export default function Demo(props) {
             defaultTabs={tabs}
             dark={dark}
             defaultCurrent={current}
-            onClick={id => setCurrent(id)}
-            onClose={id => onClose(id)}
+            onClick={key => setCurrent(key)}
+            onClose={key => onClose(key)}
           />
           <div className="chrome-tabs-optional-shadow-below-bottom-bar"></div>
           <div className="mock-browser-content">
